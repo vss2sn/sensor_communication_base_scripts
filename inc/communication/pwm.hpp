5a11b86728e5533b0d8c3afb5764672a8aa2a605
vss <28676655+vss2sn@users.noreply.github.com>
2020-10-02 16:34:09 -0400

    Refactor pwm

--- inc/communication/pwm.hpp
+++ inc/communication/pwm.hpp
@@ -1,23 +1,20 @@
-class PWM{
-  int pwm_pin_num = 0;
-  int pwm_chip_num = 0;
-  int pwm_period = 0;
-  int pwm_duty_cycle = 0;
-  int default_pwm_duty_cycle = 0;
-  int max = 170000;
-  int min = 120000;
+#include <string>
 
-
+class PWM {
+private:
+  int chip_num_ = 0;
+  int period_ = 0;
+  int duty_cycle_ = 0;
+  std::string polarity_;
 public:
-    ~PWM();
-    int create_pwm();
-    int export_pwm();
-    int unexport_pwm();
-    int set_period_pwm();
-    int set_duty_cycle_pwm();
-    int set_polarity_pwm(std::string tag = "normal");
-    int enable_pwm(std::string tag = "1");
-    int disable_pwm();
-    void set_val(std::string tag);
-    void set_default();
+    PWM(const int);
+    void createPwm();
+    void exportPwm();
+    void unexportPwm();
+    void setPeriodPwm(const int period);
+    void setDutyCyclePwm(const int duty_cycle);
+    void setPolarityPwm(const std::string& tag = "normal");
+    void enablePwm(const std::string& polarity);
+    void disablePwm();
+    void setChipNum(const int chip_num);
 };
--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,102 +1,79 @@
 /*******************************************************************************
  PWM common functions
 *******************************************************************************/
+#include <fstream>
+#include <iostream>
+#include <string>
+
 #include "communication/pwm.hpp"
 
-PWM::~PWM(){std::cout << "PWM destructor" << std::endl;}
+PWM::PWM(const int chip_num) : chip_num_(chip_num) {}
 
-int PWM::export_pwm() {
-  std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) + "/export";
+void PWM::exportPwm() {
+  const std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) + "/export";
   std::ofstream exportpwm(export_str.c_str());
   exportpwm << "0";
   exportpwm.close();
-  return 0;
 }
 
-int PWM::unexport_pwm() {
-  std::string unexport_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
+void PWM::unexportPwm() {
+  const std::string unexport_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
 			     "/export";
   std::ofstream unexportpwm(unexport_str.c_str());
   unexportpwm << "0";
   unexportpwm.close();
-  return 0;
 }
 
-int PWM::set_period_pwm() {
-  std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
-			   "/pwm" + std::to_string(pwm_chip_num) +
+void PWM::setPeriodPwm(const int period) {
+  period_ = period;
+  const std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
+			   "/pwm" + std::to_string(chip_num_) +
 			   "/period";
-  std::ofstream period_pwm(
-      export_str.c_str());
-  period_pwm << pwm_period;
+  std::ofstream period_pwm(export_str.c_str());
+  period_pwm << period_;
   period_pwm.close();
-  return 0;
 }
 
-int PWM::set_duty_cycle_pwm() {
-  std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
-                           "/pwm" + std::to_string(pwm_chip_num) +
-                           "/duty_cycle"; //"/pwm"+std::to_string(number)+"/polarity"
-  std::ofstream duty_cycle_pwm(
-      export_str.c_str());
-  duty_cycle_pwm << pwm_duty_cycle;
+void PWM::setDutyCyclePwm(const int duty_cycle) {
+  duty_cycle_ = duty_cycle;
+  const std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
+                           "/pwm" + std::to_string(chip_num_) +
+                           "/duty_cycle";
+  std::ofstream duty_cycle_pwm(export_str.c_str());
+  duty_cycle_pwm << duty_cycle_;
   duty_cycle_pwm.close();
-  return 0;
 }
 
-int PWM::enable_pwm(std::string tag) {
-  std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
-                           "/pwm" + std::to_string(pwm_chip_num) +
-                           "/enable"; //"/pwm"+std::to_string(number)+"/polarity"
-  std::ofstream enable_pwm(
-      export_str.c_str());
+void PWM::enablePwm(const std::string& tag) {
+  const std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
+                           "/pwm" + std::to_string(chip_num_) +
+                           "/enable";
+  std::ofstream enable_pwm(export_str.c_str());
   enable_pwm << tag;
   enable_pwm.close();
-  return 0;
 }
 
-int PWM::disable_pwm() {
-  std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
-                           "/pwm" + std::to_string(pwm_chip_num) +
-                           "/enable"; //"/pwm"+std::to_string(number)+"/polarity"
-  std::ofstream disable_pwm(
-      export_str.c_str());
+void PWM::disablePwm() {
+  const std::string export_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
+                           "/pwm" + std::to_string(chip_num_) +
+                           "/enable";
+  std::ofstream disable_pwm(export_str.c_str());
   disable_pwm << "0";
   disable_pwm.close();
-  return 0;
 }
 
-int PWM::set_polarity_pwm(std::string tag) {
-  std::string polarity_str = "/sys/class/pwm/pwmchip" + std::to_string(pwm_chip_num) +
-		 	   "/pwm" + std::to_string(pwm_chip_num) +
+void PWM::setPolarityPwm(const std::string& polarity) {
+  polarity_ = polarity;
+  const std::string polarity_str = "/sys/class/pwm/pwmchip" + std::to_string(chip_num_) +
+		 	   "/pwm" + std::to_string(chip_num_) +
 			   "/polarity";
   std::ofstream polarity_pwm(polarity_str.c_str());
-  polarity_pwm << tag; //change to inverse if reading
+  polarity_pwm << polarity_; //change to inverse if reading
   polarity_pwm.close();
-  return 0;
 }
 
-// NOTE: Not using constructor as this allows alterations to settings on the fly before setting pin to enable.
-//TODO: create wrapper
-int PWM::create_pwm() {
-  export_pwm();
-  set_period_pwm();
-  set_duty_cycle_pwm();
-  enable_pwm();
-  return 0;
-}
-
-void PWM::set_val(std::string tag) {
-  // NOTE: this can be used directly to set the duty cycle as a percentage of
-  // the pulse or in microsceonds. Make sure pwm_duty_cycle is always less then
-  // the pwm_period or it will not work.
-  int number = stoi(tag);
-  pwm_duty_cycle = number;
-  set_duty_cycle_pwm();
-}
-void PWM::set_default() {
-  pwm_duty_cycle = default_pwm_duty_cycle;
-  set_duty_cycle_pwm();
+void PWM::setChipNum(const int chip_num) {
+  chip_num_ = chip_num;
 }
 
 #ifdef BUILD_INDIVIDUAL
