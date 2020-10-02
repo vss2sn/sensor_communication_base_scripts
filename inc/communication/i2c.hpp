43e7aa2f1f7664f9796251ab058e6299f5403a55
vss <28676655+vss2sn@users.noreply.github.com>
2020-10-02 16:00:25 -0400

    Refactor i2c. TODO: Fix

--- inc/communication/i2c.hpp
+++ inc/communication/i2c.hpp
@@ -1,19 +1,22 @@
 /*******************************************************************************
 I2C
 *******************************************************************************/
+#include <string>
+
 #include <linux/i2c-dev.h>
 
 class I2C{
-	std::string dev="/dev/i2c-0"; 	// Device
+	std::string dev_; 	// Device
 	int fd;
 	int slave_addr; // Slave address
-	int length;     // Number of expected bytes in read/write operation
-	char buffer[];  // Buffer ot store read values
+	int buffer_size;
+	char buffer[255];  // Buffer ot store read values
 	int op_n;   // Number of actual bytes in read/write operation
 public:
-	I2C();
-	~I2C();
-	bool check_i2c();
-	bool read_i2c();
-	bool write_i2c(std::string tag);
+	I2C(const std::string& dev);
+	bool openDevice();
+	void setDevice(const std::string& dev);
+	bool checkI2c();
+	std::tuple<bool, std::string> readI2c(const int n_bytes);
+	bool writeI2c(const std::string& tag);
 };
--- src/i2c.cpp
+++ src/i2c.cpp
@@ -3,19 +3,36 @@ References:
 https://www.kernel.org/doc/Documentation/i2c/dev-interfac
 */
 
+#include <fcntl.h> // for O_RDWR
+#include <iostream>
+#include <string.h> // for strerror
+#include <sys/ioctl.h> // for ioctl
+#include <unistd.h> // for write
+#include <string>
+#include <tuple>
+
 #include "communication/i2c.hpp"
 
-I2C::I2C(){
-	/*
-	If reading from another file / manually setting, set here
-	*/
-	fd = open(dev.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+I2C::I2C(const std::string& dev)
+	: dev_(dev) {
+		buffer_size = strlen(buffer);
+	}
+
+bool I2C::openDevice() {
+	fd = open(dev_.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
 	if (fd < 0){
-		std::cout << "Error opening " << dev << ":" << strerror(errno) << std::endl;;
+		std::cout << "Error opening " << dev_ << ":" << strerror(errno) << std::endl;
+		return false;
+	} else {
+		return true;
 	}
 }
 
-bool I2C::check_i2c(){
+void I2C::setDevice(const std::string& dev) {
+	dev_ = dev;
+}
+
+bool I2C::checkI2c(){
 	if(ioctl(fd, I2C_SLAVE, slave_addr) < 0){
 		std::cout << "Failed to aquire bus access or talk to slave" << std::endl;;
 		return false;
@@ -23,20 +40,27 @@ bool I2C::check_i2c(){
 	return true;
 }
 
-bool I2C::read_i2c(){
-	op_n = read(fd, buffer, length);
-	if(op_n !=length){
-		std::cout << "Failed to read expected number of bytes. Read " << op_n << " instead of " << length << std::endl;
-		std::cout << "Read: " << buffer << std::endl;
-		return false;
+std::tuple<bool, std::string> I2C::readI2c(const int n_bytes){
+	if(n_bytes > buffer_size) {
+		std::cout << "Failed to read expected number of bytes" << std::endl;
+		// return {false, std::string()};
 	}
-	return true;
+
+	op_n = read(fd, buffer, n_bytes);
+	std::string read_buffer(buffer);
+	read_buffer.erase(n_bytes-1, std::string::npos);
+	if(op_n != n_bytes){
+		std::cout << "Failed to read expected number of bytes. Read " << op_n << " instead of " << n_bytes << std::endl;
+		std::cout << "Read: " << read_buffer << std::endl;
+		// return {false, std::string()};
+	}
+	return {true, read_buffer};
 }
 
-bool I2C::write_i2c(std::string tag){
-	length = tag.length();
+bool I2C::writeI2c(const std::string& tag){
+	const int length = tag.length();
 	op_n = write(fd, tag.c_str(), length);
-	if (op_n!=length){
+	if (op_n != length){
 		std::cout << "Failed to write expected number of bytes. Wrote " << op_n << " instead of " << length << std::endl;
 		return false;
 	}
